32c2c944bb85a27e8b6519342cd4b5fd3b2fec71
hossmi <hossmi@gmail.com>
2019-09-15 09:18:24 +0200

    implementing DbMigrator

--- Blaxpro.Sql/DbMigrator.cs
+++ Blaxpro.Sql/DbMigrator.cs
@@ -4,9 +4,16 @@ namespace Blaxpro.Sql
 {
     public class DbMigrator : IDbMigrator
     {
+        private readonly IList<IMigration> migrations;
+
+        public DbMigrator()
+        {
+            this.migrations = new List<IMigration>();
+        }
+
         public void add(IMigration migration)
         {
-            throw new System.NotImplementedException();
+            this.migrations.Add(migration);
         }
 
         public IEnumerable<IMigrationScript> getScripts(int fromVersion, int toVersion)
@@ -16,7 +23,16 @@ namespace Blaxpro.Sql
 
         public IMigrationResult upgrade(IDb db)
         {
+            PrvMigrationStep migrationStep;
+
+            migrationStep = prv_getMigrationsSequence(this.migrations);
+
+
             throw new System.NotImplementedException();
+        }
+
+        private class PrvMigrationStep
+        {
         }
     }
 }
